69cccb16b1be2e8c5f0bb18546a17d2477f5dace
oselumeseagbonrofo <oselumeseagbonrofo@gmail.com>
2022-10-20 17:38:41 +0000

    work

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return:returns 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+Return(0);
+}
