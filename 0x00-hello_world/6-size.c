77ddec092237881ae43ed5ab29000b9714a7a97c
oselumeseagbonrofo <oselumeseagbonrofo@gmail.com>
2022-10-21 14:59:07 +0000

    g

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,6 +11,6 @@ int main(void)
 	  printf("Size of a long int: %i byte(s)\n", sizeof(long int));
 	   printf("Size of a long long int: %u byte(s)\n", sizeof(long long int));
 	     printf("Size of a float: %i byte(s)\n", sizeof(float));
-	     fprintf(stderr, "sad\n");
+	     fprintf(stderr,"my\n");
 return (0);
 }
